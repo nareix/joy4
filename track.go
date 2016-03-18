7f98b6983417ae3162f4ccffdf5d26365ac51b3f
nareix <hi.nareix@gmail.com>
2016-03-18 20:00:20 +0800

    add track.go

---
+++ track.go
@@ -0,0 +1,27 @@
+
+package mp4
+
+import (
+	"github.com/nareix/mp4/atom"
+	"io"
+)
+
+const (
+	H264 = 1
+	AAC = 2
+)
+
+type Track struct {
+	Type int
+	SPS []byte
+	PPS []byte
+	TrackAtom *atom.Track
+	r io.ReadSeeker
+
+	sample *atom.SampleTable
+	sampleIndex int
+	chunkGroupIndex int
+	chunkIndex int
+	sampleIndexInChunk int
+}
+
