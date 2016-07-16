040331c05980d89f20d32fa72bfe34ef8c0b82d7
nareix <hi.nareix@gmail.com>
2016-07-16 13:26:54 +0800

    add audio_decode.go

---
+++ examples/audio_decode.go
@@ -0,0 +1,40 @@
+
+package main
+
+import (
+	"github.com/nareix/joy4/av"
+	"github.com/nareix/joy4/format"
+	"github.com/nareix/joy4/av/avutil"
+	"github.com/nareix/joy4/cgo/ffmpeg"
+)
+
+// need ffmpeg installed
+
+func init() {
+	format.RegisterAll()
+}
+
+func main() {
+	file, _ := avutil.Open("projectindex.flv")
+	streams, _ := file.Streams()
+	var dec *ffmpeg.AudioDecoder
+
+	for _, stream := range streams {
+		if stream.Type() == av.AAC {
+			dec, _ = ffmpeg.NewAudioDecoder(stream.(av.AudioCodecData))
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		pkt, _ := file.ReadPacket()
+		if streams[pkt.Idx].Type() == av.AAC {
+			ok, frame, _ := dec.Decode(pkt.Data)
+			if ok {
+				println("decode samples", frame.SampleCount)
+			}
+		}
+	}
+
+	file.Close()
+}
+
