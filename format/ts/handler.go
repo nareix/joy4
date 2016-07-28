d5c3f87b5cc4a48112088efe36ae34b548d7799a
nareix <hi.nareix@gmail.com>
2016-07-29 07:50:21 +0800

    ts: add probe func

--- format/ts/handler.go
+++ format/ts/handler.go
@@ -9,6 +9,10 @@ import (
 func Handler(h *avutil.RegisterHandler) {
 	h.Ext = ".ts"
 
+	h.Probe = func(b []byte) bool {
+		return b[0] == 0x47 && b[188] == 0x47
+	}
+
 	h.ReaderDemuxer = func(r io.Reader) av.Demuxer {
 		return NewDemuxer(r)
 	}
