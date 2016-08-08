0d5a2fa0f0cf9e0495428d839d05b65efc176909
nareix <hi.nareix@gmail.com>
2016-08-08 17:12:04 +0800

    add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+
+// Package joy4 is a Golang audio/video library and streaming server.
+// JOY4 is powerful library written in golang, well-designed interface makes a few lines 
+// of code can do a lot of things such as reading, writing, transcoding among 
+// variety media formats, or setting up high-performance live streaming server.
+package joy4
