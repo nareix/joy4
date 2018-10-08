08d0b6d09dfeab0501aa083cf443035a77fcbfa6
Antoine <antoou@gmail.com>
2018-10-08 11:17:56 +0200

    feat(video.go): restore gop size and bitrate setting

--- cgo/ffmpeg/video.go
+++ cgo/ffmpeg/video.go
@@ -14,7 +14,6 @@ import (
 	"fmt"
 	"image"
 	"reflect"
-
 	"github.com/nareix/joy4/av"
 	"github.com/nareix/joy4/codec/h264parser"
 )
@@ -176,39 +175,8 @@ func (enc *VideoEncoder) Setup() (err error) {
 
 	ff.codecCtx.time_base.num	= C.int(enc.fpsDen)
 	ff.codecCtx.time_base.den	= C.int(enc.fpsNum)
-	// ff.codecCtx.ticks_per_frame	= 2;
-
-	// ff.codecCtx.gop_size		= C.int(enc.gopSize)
-	// ff.codecCtx.max_b_frames	= C.int(3)
-	// keyint_min
-	// ff.codecCtx.refs			= C.int(1)
-
-	// ff.codecCtx.bit_rate		= C.int64_t(enc.Bitrate)
-	// ff.codecCtx.bit_rate_tolerance = C.int(ff.codecCtx.bit_rate/10);
-	// ff.codecCtx.rc_max_rate		= ff.codecCtx.bit_rate
-	// ff.codecCtx.rc_min_rate		= ff.codecCtx.bit_rate
-	// ff.codecCtx.rc_buffer_size	= C.int(ff.codecCtx.bit_rate)
-
-	// ff.codecCtx.profile			= C.FF_PROFILE_H264_BASELINE
-	// ff.codecCtx.level			= C.int(31)
-
-
-	// err = enc.SetOption("preset", "slow")
-	// if err != nil {
-	// 	fmt.Println("Error while setting preset, err", err)
-	// }
-	// err = enc.SetOption("tune", "zerolatency")
-	// if err != nil {
-	// 	fmt.Println("Error while setting preset, err", err)
-	// }
-
-	// disable b-pyramid. CLI options for this is "-b-pyramid 0"
-	// Because Quicktime (ie. iOS) doesn't support this option
-	// cerr = C.av_opt_set(ff.codecCtx.priv_data, "b-pyramid", "0", 0)
-	// err = enc.SetOption("b-pyramid", "0")
-	// if err != nil {
-	// 	fmt.Println("Error while setting b-pyramid, err", err)
-	// }
+	ff.codecCtx.gop_size		= C.int(enc.gopSize)
+	ff.codecCtx.bit_rate		= C.int64_t(enc.Bitrate)
 
 	if C.avcodec_open2(ff.codecCtx, ff.codec, nil) != 0 {
 		err = fmt.Errorf("ffmpeg: encoder: avcodec_open2 failed")
